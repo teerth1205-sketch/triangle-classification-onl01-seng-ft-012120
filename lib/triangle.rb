3ed9d2e5440bee4846035f5c6c90f5ab46c62cbb
Teerth Patel <teerth1205@gmail.com>
2020-02-06 21:41:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,11 @@
 class Triangle
-  # triangle code
+ def initialize(side1,side2,side3)
+   @side1 = side1
+   @side2 = side2
+   @side3 = side3
+ end 
+ 
+ def kind 
  
   class TriangleError < StandardError
     # triangle error code
