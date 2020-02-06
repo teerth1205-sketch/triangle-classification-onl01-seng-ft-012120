c697ce05621f3031783f3230c410fad5c1853330
Teerth Patel <teerth1205@gmail.com>
2020-02-06 21:49:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
    if @side1 == @side2 && @side2 == @side3
      :equilateral
    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
-      
+      :isosceles
    end 
    
  end 
